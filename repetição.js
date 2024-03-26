50b5a8f7cbacd6f273a4a2100e3f67adcc28e6ac
Leryane <138252093+Leeryane@users.noreply.github.com>
2024-03-26 09:20:56 -0300

    aprendendo sobre repetições em js

---
+++ repetição.js
@@ -0,0 +1,23 @@
+var c = 6
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+
+
+/* var c = 1
+while (c <= 6) {
+    console.log('Tudo bem?')
+    c++
+} */
+
+
+/* 
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?') */

055c7498e87e8493f1dd593615b56238b3150045
Leryane <138252093+Leeryane@users.noreply.github.com>
2024-03-26 09:21:16 -0300

    atualizei os exercicios

--- EXERCICIO2/script.js
+++ EXERCICIO2/script.js
@@ -8,6 +8,49 @@ function verificar () {
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fAno.value)
-        res.innerHTML = `Idade calculada: ${idade}`	
+        var genero = ''
+        var img= document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >=0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'bebe-homem.png')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'homemjovem.png')
+            } else if (idade < 50) {
+                //  Adulto
+                img.setAttribute('src', 'homemadulto.png')
+            } else  {
+                // Idoso
+                img.setAttribute('src', 'iodosohomem.png')
+            }
+      
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >=0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'mulherbebe.png')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'mulherjovem.png')
+            } else if (idade < 50) {
+                //  Adulto
+                img.setAttribute('src', 'mulheradulta.png')
+            } else  {
+                // Idoso
+                img.setAttribute('src', 'mulheridosa.png')
+            }
+    }
+    res.style.textAlign = 'center'
+    res.innerHTML = `<p>Detectamos ${genero} com ${idade} anos.</p>`
+    res.appendChild(img)
+   
+   
+    
 }
+
+
+
 }
